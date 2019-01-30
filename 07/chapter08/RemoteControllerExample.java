1fb5f3d94fdc40d84d520f9c2de32496cc5074e1
chaminji <minjicha0319@gmail.com>
2019-01-30 13:02:41 +0900

    Interface_code+md(3/7)

---
+++ 07/chapter08/RemoteControllerExample.java
@@ -0,0 +1,15 @@
+package chapter08;
+
+public class RemoteControllerExample {
+	public static void main(String[] args) {
+//		RomoteControl rc = new RomoteControl() {
+//			public void turnOn() {/*실행문*/}
+//			public void turnOff() {/*실행문*/}
+//			public void setVolume(int volume) {/*실행문*/}
+//		};	
+		
+		//Television과 Audio 객체 사용
+		/*rc = new Television();
+		rc = new Audio();*/
+	}
+}
